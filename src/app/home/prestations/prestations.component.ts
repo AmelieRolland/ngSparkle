6b3cf8d3d47f60a6579d179618d19aab389384b0
Amélie Rolland <amelierolland.v@gmail.com>
2024-09-30 11:07:42 +0200

    personnel associe

--- src/app/home/prestations/prestations.component.ts
+++ src/app/home/prestations/prestations.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-prestations',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './prestations.component.html',
   styleUrl: './prestations.component.css'
 })
